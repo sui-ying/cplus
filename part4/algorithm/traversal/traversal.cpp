b1557ddfb485b236586f39f33c8eef48a55ce187
sui-ying <xyc252709151@163.com>
2023-09-11 18:48:46 +0800

    add traversal

--- part4/algorithm/traversal/traversal.cpp
+++ part4/algorithm/traversal/traversal.cpp
@@ -17,9 +17,17 @@ public:
 
 };
 
+class Transform
+{
+public:
+    int operator()(int val){
+        return -val;
+    }
+};
+
 void test_traversal(){
     vector<int>v = {2, 3, 1, 5, 4};
-
+    // Todo
     for_each(v.begin(), v.end(), print01);   // 传入普通函数 
     cout << endl;
 
@@ -27,8 +35,20 @@ void test_traversal(){
     cout << endl;
 }
 
+void test_transform(){
+    vector<int>v = {2, 3, 1, 5, 4};
+    vector<int>v2;
+    v2.resize(v.size());
+    
+    transform(v.begin(), v.end(), v2.begin(), Transform());
+    for_each(v2.begin(), v2.end(), print01);   // 传入普通函数 
+    cout << endl;
+
+
+}
 
 int main(){
     test_traversal();
+    test_transform();
     return 0;
 }
